fa2584f12da1b0912e54966606a1448b9eb89e8c
aemr3 <emres@petlantic.com>
2016-01-24 00:55:22 +0200

    Updated network options

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,99 +2,28 @@
 
 echo 'Starting compute service...'
 
-sed -i "s/^#net.ipv4.conf.all.rp_filter.*/net.ipv4.conf.all.rp_filter=0/" /etc/sysctl.conf
-sed -i "s/^#net.ipv4.conf.default.rp_filter.*/net.ipv4.conf.default.rp_filter=0/" /etc/sysctl.conf
-sysctl -p /etc/sysctl.conf
+sed -i "\
+  s/^# rpc_backend=rabbit.*/rpc_backend=rabbit/; \
+  s/^# rabbit_host = localhost.*/rabbit_host=$CONTROLLER_HOST/; \
+  s/^# rabbit_userid = guest.*/rabbit_userid = $RABBIT_USER/; \
+  s/^# rabbit_password = guest.*/rabbit_password = $RABBIT_PASS/; \
+  s/^# auth_strategy = keystone.*/auth_strategy = keystone/; \
+  s/^auth_uri =.*/auth_uri = http:\/\/$CONTROLLER_HOST:5000/; \
+  s/^identity_uri =.*/auth_url = http:\/\/$CONTROLLER_HOST:35357/; \
+  s/^admin_tenant_name =.*/auth_plugin = password\nproject_domain_id = default\nuser_domain_id = default\nproject_name = service/; \
+  s/^admin_user =.*/username = neutron/; \
+  s/^admin_password =.*/password = $NEUTRON_PASS/; \
+" /etc/neutron/neutron.conf
 
-NEUTRON_CONF=/etc/neutron/neutron.conf
-ML2_CONF=/etc/neutron/plugins/ml2/ml2_conf.ini
-L3_AGENT=/etc/neutron/l3_agent.ini
-METADATA_AGENT=/etc/neutron/metadata_agent.ini
-
-sed -i "s/^# rpc_backend=rabbit.*/rpc_backend=rabbit/" $NEUTRON_CONF
-sed -i "s/^# rabbit_host = localhost.*/rabbit_host=$CONTROLLER_HOST/" $NEUTRON_CONF
-sed -i "s/^# rabbit_userid = guest.*/rabbit_userid = $RABBIT_USER/" $NEUTRON_CONF
-sed -i "s/^# rabbit_password = guest.*/rabbit_password = $RABBIT_PASS/" $NEUTRON_CONF
-sed -i "s/^# auth_strategy = keystone.*/auth_strategy = keystone/" $NEUTRON_CONF
-sed -i "s/^auth_uri =.*/auth_uri = http:\/\/$CONTROLLER_HOST:5000/" $NEUTRON_CONF
-sed -i "s/^identity_uri =.*/auth_url = http:\/\/$CONTROLLER_HOST:35357/" $NEUTRON_CONF
-sed -i "s/^admin_tenant_name =.*/auth_plugin = password\nproject_domain_id = default\nuser_domain_id = default\nproject_name = service/" $NEUTRON_CONF
-sed -i "s/^admin_user =.*/username = neutron/" $NEUTRON_CONF
-sed -i "s/^admin_password =.*/password = $NEUTRON_PASS/" $NEUTRON_CONF
-sed -i "s/^# service_plugins.*/service_plugins = router/" $NEUTRON_CONF
-sed -i "s/^# allow_overlapping_ips.*/allow_overlapping_ips = True/" $NEUTRON_CONF
-sed -i "s/# agent_down_time = 75.*/agent_down_time = 75/" $NEUTRON_CONF
-sed -i "s/# report_interval = 30.*/report_interval = 5/" $NEUTRON_CONF
-
-if [ "$HA_MODE" == "DVR" ]; then
-    sed -i "s/^# router_distributed.*/router_distributed = True/" $NEUTRON_CONF
-else
-    sed -i "s/^# router_distributed.*/router_distributed = False/" $NEUTRON_CONF
-fi
-
-sed -i "s/^# type_drivers.*/type_drivers = flat,vxlan/" $ML2_CONF
-sed -i "s/^# tenant_network_types.*/tenant_network_types = vxlan/" $ML2_CONF
-sed -i "s/^# mechanism_drivers.*/mechanism_drivers = openvswitch,l2population/" $ML2_CONF
-sed -i "s/^# vni_ranges.*/vni_ranges = 1:1000/" $ML2_CONF
-sed -i "s/^# vxlan_group.*/vxlan_group = 239.1.1.1/" $ML2_CONF
-sed -i "s/^# enable_security_group.*/enable_security_group = True/" $ML2_CONF
-sed -i "s/^# enable_ipset.*/enable_ipset = True/" $ML2_CONF
-sed -i "s/^# flat_networks.*/flat_networks = external/" $ML2_CONF
-echo "firewall_driver = neutron.agent.linux.iptables_firewall.OVSHybridIptablesFirewallDriver" >> $ML2_CONF
-
-echo "" >> $ML2_CONF
-echo "[ovs]" >> $ML2_CONF
-echo "local_ip = $TUNNEL_IP" >> $ML2_CONF
-echo "enable_tunneling = True" >> $ML2_CONF
-echo "bridge_mappings = external:br-ex" >> $ML2_CONF
-
-echo "" >> $ML2_CONF
-echo "[agent]" >> $ML2_CONF
-echo "l2population = True" >> $ML2_CONF
-echo "tunnel_types = vxlan" >> $ML2_CONF
-
-if [ "$HA_MODE" == "DVR" ]; then
-    echo "enable_distributed_routing = True" >> $ML2_CONF
-else
-    echo "enable_distributed_routing = False" >> $ML2_CONF
-fi
-
-echo "arp_responder = True" >> $ML2_CONF
-
-sed -i "s/^# interface_driver = neutron.agent.linux.interface.OVSInterfaceDriver.*/interface_driver = neutron.agent.linux.interface.OVSInterfaceDriver/" $L3_AGENT
-sed -i "s/^# use_namespaces.*/use_namespaces = True/" $L3_AGENT
-sed -i "s/^# router_delete_namespaces =.*/router_delete_namespaces = True/" $L3_AGENT
-
-if [ "$HA_MODE" == "DVR" ]; then
-    sed -i "s/^# agent_mode.*/agent_mode = dvr/" $L3_AGENT
-else
-    sed -i "s/^# agent_mode.*/agent_mode = legacy/" $L3_AGENT
-fi
-
-sed -i "s/^auth_url.*/auth_url = http:\/\/$CONTROLLER_HOST:5000\/v2.0/" $METADATA_AGENT
-sed -i "s/^auth_region.*/auth_region = $REGION_NAME/" $METADATA_AGENT
-sed -i "s/^admin_tenant_name.*/admin_tenant_name = service/" $METADATA_AGENT
-sed -i "s/^admin_user.*/admin_user = neutron/" $METADATA_AGENT
-sed -i "s/^admin_password.*/admin_password = $NEUTRON_PASS/" $METADATA_AGENT
-sed -i "s/^# nova_metadata_ip.*/nova_metadata_ip = $CONTROLLER_HOST/" $METADATA_AGENT
-sed -i "s/^# metadata_proxy_shared_secret.*/metadata_proxy_shared_secret = $METADATA_SECRET/" $METADATA_AGENT
-
-service openvswitch-switch restart
-
-ifconfig br-ex
-if [ $? != 0 ]; then
-    echo 'Making br-ex bridge using OVS command'
-    ovs-vsctl add-br br-ex
-fi
-
-if [ "$INTERFACE_NAME" ]; then
-    echo 'Add port to br-ex bridge....'
-    ovs-vsctl add-port br-ex $INTERFACE_NAME
-fi
-
-service neutron-plugin-openvswitch-agent restart
-service neutron-metadata-agent restart
-service neutron-l3-agent restart
+sed -i "\
+  s/# physical_interface_mappings.*/physical_interface_mappings = public:$INTERFACE_NAME/; \
+  s/# enable_vxlan.*/enable_vxlan = True/; \
+  s/# local_ip.*/local_ip = $LOCAL_IP/; \
+  s/# l2_population.*/l2_population = True/; \
+  s/^\[agent\]/[agent]\n\nprevent_arp_spoofing = True/; \
+  s/# enable_security_group.*/enable_security_group = True/; \
+  s/# firewall_driver.*/neutron.agent.linux.iptables_firewall.IptablesFirewallDriver/; \
+" /etc/neutron/plugins/ml2/linuxbridge_agent.ini
 
 NOVA_CONF=/etc/nova/nova.conf
 NOVA_COMPUTE=/etc/nova/nova-compute.conf
@@ -103,7 +32,7 @@ echo "my_ip = $LISTEN_IP" >> $NOVA_CONF
 echo "auth_strategy = keystone" >> $NOVA_CONF
 echo "network_api_class = nova.network.neutronv2.api.API" >> $NOVA_CONF
 echo "security_group_api = neutron" >> $NOVA_CONF
-echo "linuxnet_interface_driver = nova.network.linux_net.LinuxOVSInterfaceDriver" >> $NOVA_CONF
+echo "linuxnet_interface_driver = nova.network.linux_net.NeutronLinuxBridgeInterfaceDriver" >> $NOVA_CONF
 echo "firewall_driver = nova.virt.firewall.NoopFirewallDriver" >> $NOVA_CONF
 echo "fixed_ip_disassociate_timeout=30" >> $NOVA_CONF
 echo "enable_instance_password=False" >> $NOVA_CONF
@@ -191,6 +120,7 @@ fi
 
 service libvirt-bin restart
 service nova-compute restart
+service neutron-plugin-linuxbridge-agent restart
 
 ## Setup complete
 echo 'Setup complete!...'
